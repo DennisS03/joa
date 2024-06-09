ee10401caea9adfb83c6cab9e2ddfdd0f5f71c72
DennisS03 <61012497+DennisS03@users.noreply.github.com>
2024-06-09 17:26:47 +0200

    feat: Added link to landing page

--- src/lib/views/Header.svelte
+++ src/lib/views/Header.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import LogoGithub from 'carbon-icons-svelte/lib/LogoGithub.svelte';
+	import Download from 'carbon-icons-svelte/lib/Download.svelte';
 </script>
 
 <div id="header">
-	<a href="https://github.com/DennisS03/joa">
-		<LogoGithub size={32} />
-	</a>
+	<a href="/">Startseite</a>
+	<div>
+		{#if $page.route.id === '/article'}
+			<a href="/article/download?sources=true" download>
+				<Download size={32} />
+				Quellen
+			</a>
+			<a href="/article/download" download>
+				<Download size={32} />
+				Artikel
+			</a>
+		{/if}
+		<a href="https://github.com/DennisS03/joa">
+			<LogoGithub size={32} />
+		</a>
+	</div>
 </div>
 
 <style lang="scss">
 	#header {
 		display: flex;
+		gap: 1rem;
 		position: sticky;
 		top: 0;
 		height: 50px;
@@ -20,10 +36,17 @@
 		padding: 0 2rem;
 		box-sizing: border-box;
 		align-items: center;
-		justify-content: right;
+		justify-content: space-between;
 
 		a {
+			display: flex;
+			align-items: center;
+			gap: 0.5rem;
 			color: black;
+
+			&:link {
+				text-decoration: none;
+			}
 		}
 	}
 </style>
